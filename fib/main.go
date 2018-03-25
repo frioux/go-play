9f626333b5bb031335255e5d1b517783d090213d
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2018-03-25 14:09:08 -0700

    yuge

--- fib/main.go
+++ fib/main.go
@@ -1,16 +1,17 @@
 package main
 
 import "fmt"
+import "math/big"
 
 func main() {
-	c := make(chan int, 2)
-	c <- 1
-	c <- 0
+	c := make(chan *big.Int, 2)
+	c <- big.NewInt(1)
+	c <- big.NewInt(0)
 
 	for {
 		n := <-c
 		fmt.Println(n)
-		c <- n + <-c
+		c <- big.NewInt(0).Add(n, <-c)
 		c <- n
 	}
 }
